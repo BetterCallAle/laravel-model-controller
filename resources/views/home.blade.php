f39c691d53cd4d4ba01b256deddcf4a3133fe310
BetterCallAle <dap22091998@gmail.com>
2022-12-16 15:39:42 +0100

    the movies are visible in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,33 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('title')
     Movies
 @endsection
 
 @section('content')
-    <h1>Pagina Home</h1>
+   <section>
+    <div class="container">
+        <div class="row row-cols-3 gy-5">
+            @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card">
+                    <div class="card-img">
+                         <img src="{{ $movie->image }}" alt="Copertina di {{ $movie->title }}">
+                    </div>
+     
+                    <div class="card-info">
+                        <ul>
+                             <li>{{ $movie->title }}</li>
+                             <li>{{ $movie->original_title }}</li>
+                             <li>{{ $movie->nationality }}</li>
+                             <li>{{ $movie->date }}</li>
+                             <li>{{ $movie->vote }}</li>
+                         </ul>
+                    </div>
+                </div>
+            </div>
+             @endforeach
+        </div>
+    </div>
+   </section>
 @endsection
