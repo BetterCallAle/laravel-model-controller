1b8b943d6d50a6ab0088e45d194d553a38eb2b99
BetterCallAle <dap22091998@gmail.com>
2022-12-16 16:04:28 +0100

    added a link to home in the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,9 @@
 <header>
     <div class="container">
         <div class="logo">
-            <h1>FakeFlix 2.0</h1>
+            <a href="{{ route('home') }}">
+                <h1>FakeFlix 2.0</h1>
+            </a>
         </div>
     </div>
 </header>
